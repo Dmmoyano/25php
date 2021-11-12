ec455772b0a60692039275088d917fd98c47f29c
aadriaan98 <adrianricardo123barros1@gmail.com>
2021-11-12 12:45:03 -0300

    primer commit

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create(Request $request)
+    {
+        try{
+            Usuario::create([
+                
+                'name' => $request->name,
+                'email'=>$request->email,
+                'phone' => $request->phone,
+                'message'=> $request->message,
+            ]);
+
+            $details = [
+                'title' => 'Post title: ' . $request->title,
+                'body' => $request->description
+            ];
+            \Mail::to('your_receiver_email@gmail.com')->send(new \App\Mail\sendPost($details));
+
+            return json_encode(['status'=>'ok']);
+
+        }catch(\ErrorException $e){
+            return json_encode(['status'=>'faild', 'message'=>$e->getMessage()]);
+        }
+    }
+
+}
